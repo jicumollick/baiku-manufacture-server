d4c656dff89e112a873c247507ff6c936dcdba3e
jicu mollick <jikumollick82@gmail.com>
2022-05-24 20:35:37 +0600

    backend setup Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello from baiku");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+var cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilere.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  //   client.close();
+  console.log("database connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
